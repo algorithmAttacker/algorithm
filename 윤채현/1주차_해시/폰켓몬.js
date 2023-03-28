0eb069014a4983f28d5db3315313fad78b5c70fa
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-28 19:04:38 +0900

    add programmers 폰켓몬.js

---
+++ 윤채현/1주차_해시/폰켓몬.js
@@ -0,0 +1,13 @@
+function solution(nums) {
+    const len = nums.length;
+    const set = new Set(nums);
+
+    console.log(set);
+
+    if(set.size >= len/2) {
+        return len/2
+    }
+    else {
+        return set.size
+    }
+}
