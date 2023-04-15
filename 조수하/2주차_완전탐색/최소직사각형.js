df8b1473d302f507c54eb5c09e697c38482481e9
JoSooha <dlrhdcjs@naver.com>
2023-04-16 03:37:13 +0900

    refactor 완탐 주석

--- 조수하/2주차_완전탐색/최소직사각형.js
+++ 조수하/2주차_완전탐색/최소직사각형.js
@@ -3,6 +3,7 @@ function solution(sizes) {
     var first = 0
     var second = 0 //w h 중에 작은 것중 가장 큰 것
     
+    //각 size[a][b] 에서 ab 둘 중 큰 것을 골라 그 중 최대치, 작은 것을 골라 그 중 최대치 구해서 곱
     for(var i=0; i<sizes.length; i++) {
         if(sizes[i][0]>=sizes[i][1]){
             if(second < sizes[i][1]) second = sizes[i][1]
@@ -15,6 +16,7 @@ function solution(sizes) {
             }                
         }
     } 
+    
     answer=first*second
         
     return answer;
