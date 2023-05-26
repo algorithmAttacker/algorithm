12ab8c0315a248e3e13890859f23d36991545261
JoSooha <dlrhdcjs@naver.com>
2023-05-26 23:47:35 +0900

    add programmers 순위.js

---
+++ 조수하/7주차_이분탐색 그래프/순위.js
@@ -0,0 +1,38 @@
+function solution(n, results) {
+    // 그래프 초기화
+    const graph = Array.from({ length: n }, () => Array(n).fill(false));
+  
+    // 간선 추가
+    for (let [winner, loser] of results) {
+      graph[winner - 1][loser - 1] = true;
+    }
+  
+    // 각 선수에 대해 DFS를 통해 도달 가능한 노드 수 계산
+    const counts = Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+      const visited = Array(n).fill(false);
+      dfs(i, i, visited);
+    }
+  
+    // DFS 함수 정의
+    function dfs(start, current, visited) {
+      visited[current] = true;
+  
+      for (let i = 0; i < n; i++) {
+        if (graph[current][i] && !visited[i]) {
+          counts[start]++;
+          dfs(start, i, visited);
+        }
+      }
+    }
+  
+    // 도달 가능한 노드의 수가 n-1인 경우 순위가 확정됨
+    let answer = 0;
+    for (let count of counts) {
+      if (count === n - 1) {
+        answer++;
+      }
+    }
+  
+    return answer;
+  }
