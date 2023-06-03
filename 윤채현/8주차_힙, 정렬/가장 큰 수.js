4dcb9299599d37a24862c374b119bb7b7499dbd9
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-06-03 14:48:40 +0900

    add programmers 가장 큰 수.js

---
+++ 윤채현/8주차_힙, 정렬/가장 큰 수.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+  let answer = numbers
+    .map((ele) => ele.toString())
+    .sort((a, b) => {
+      if (b + a > a + b) {
+        return 1;
+      } else if (b + a < a + b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    })
+    .join("");
+  return answer[0] === "0" ? "0" : answer;
+}
